7085f62d76adb19f30579324ac87ead208d97a23
Irina White <irina.k.white@gmail.com>
2022-11-24 13:58:32 +0100

    trained 4 models on CV grid & select the best

--- modelling.py
+++ modelling.py
@@ -5,14 +5,50 @@ from sklearn.linear_model import SGDRegressor
 from sklearn.preprocessing import StandardScaler, scale
 from sklearn.metrics import mean_squared_error, accuracy_score
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 import matplotlib.pyplot as plt 
 import itertools
+import joblib
+import json
+import os
+import re
+import glob
 from tabular_data import load_airbnb
 
-def train_model(X,y):
+#Hyperparameteres grids
+sgd_param = {'alpha': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100],
+    'learning_rate': ['constant', 'optimal', 'invscaling', 'adaptive'],
+    'loss': ['squared_error', 'huber', 'epsilon_insensitive','squared_epsilon_insensitive'],
+    'penalty' : ['l2', 'l1', 'elasticnet'],
+    'max_iter' : [750, 1000, 1250, 1500]}
+
+decision_tree_param={"splitter":["best","random"],
+    "max_depth" : [1,3,5,7,9,11,12],
+    "min_samples_leaf":[1,2,3,4,5,6,7,8,9,10],
+    "min_weight_fraction_leaf":[0.1,0.2,0.3,0.4,0.5],
+    "max_features":[1.0, "log2","sqrt",None],
+    "max_leaf_nodes":[None,10,20,30,40,50,60,70,80,90]}
+
+random_forest_param={'bootstrap': [True, False],
+    'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
+    'max_features': [1,0, 'sqrt'],
+    'min_samples_leaf': [1, 2, 4],
+    'min_samples_split': [2, 5, 10],
+    'n_estimators': [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000]}
+
+gradient_boost_param={'n_estimators':[500,1000,2000],
+    'learning_rate':[.001,0.01,.1],
+    'max_depth':[1,2,4],
+    'subsample':[.5,.75,1],
+    'random_state':[1]}
+
+
+
+def train_model(X,y, model=SGDRegressor):
     xtrain, xtest, ytrain, ytest=train_test_split(X, y, test_size=0.2, random_state=1)
     xtrain, xval, ytrain, yval= train_test_split(xtrain, ytrain, test_size=0.25, random_state=1) # 0.25 x 0.8 = 0.2
-    model_sgdr = SGDRegressor()
+    model_sgdr = model()
     model_sgdr.fit(xtrain, ytrain)
     return model_sgdr, xtrain, ytrain, xval, yval, xtest, ytest
 
@@ -62,7 +98,7 @@ def custom_tune_regression_model_hyperparameters(model_type, grid_dic, data_sets
         if mse<best_mse:
             best_mse=mse
             performance_metrics_dict["mse"] = mse
-            performance_metrics_dict["rmse"] = val_mse**(1/2.0)
+            performance_metrics_dict["rmse"] = mse**(1/2.0)
             performance_metrics_dict["r2"] = score
             best_hyperparameter_values_dict=param
             best_model=model
@@ -78,9 +114,58 @@ def tune_regression_model_hyperparameters(model_type, grid_dic, data_sets):
     mse, score=calculate_scores(data_sets[2], data_sets[3], gs_estimator)
     performance_metrics_dict={}
     performance_metrics_dict["mse"] = mse
-    performance_metrics_dict["rmse"] = val_mse**(1/2.0)
+    performance_metrics_dict["rmse"] = mse**(1/2.0)
     performance_metrics_dict["r2"] = score
     return gs_estimator, best_hyperparameter_values_dict, performance_metrics_dict
+
+
+def evaluate_all_models(model, grid_dic, data_sets):
+    model_type=model()
+    folder=str(model_type.__class__.__name__).replace('Regressor', '')
+    new_name=re.findall('[A-Z][^A-Z]*', folder)
+    new_name='_'.join(new_name).lower()
+    model, param, metrics=tune_regression_model_hyperparameters(model, grid_dic, data_sets)
+    path='models/regression/'+new_name+'/'
+    save_model(model, param, metrics, path)
+    
+
+def save_model(model, param, metrics, path):
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
+
+    model_filename = path+'model.joblib'
+    hyperparam_filename=path+'hyperparameters.json'
+    metrics_filename=path+'metrics.json'
+    joblib.dump(model, model_filename)
+    with open(hyperparam_filename, 'w') as fp:
+        json.dump(param,fp)
+    with open(metrics_filename, 'w') as fp:    
+        json.dump(metrics, fp)
+    print ('Model is saved')
+
+
+def find_best_model():
+    #upload metrics, compare r2, take the highest
+    path = "./models/regression"
+    metrics_files = glob.glob(path + "/**/metrics.json", recursive = True)
+    best_r2=0
+    for file in metrics_files:
+        f = open(str(file))
+        metrics_dic = json.load(f)
+        r2=metrics_dic['r2']
+        if r2>best_r2:
+            best_r2=r2
+            best_name=str(file).split('/')[-2]
+    path='./models/regression/'+best_name+'/'
+    model=joblib.load(path+'model.joblib')
+    with open (path+'hyperparameters.json', 'r') as fp:
+        param=json.load(fp)
+    with open (path+'metrics.json', 'r') as fp:
+        metrics=json.load(fp)
+
+    return model, param, metrics
+
 
 
 if __name__ == "__main__":
@@ -88,31 +173,18 @@ if __name__ == "__main__":
     X, y = load_airbnb(df, 'Price_Night')
     X = scale(X)
     y = scale(y)
-    model_sgdr, xtrain, ytrain, xval, yval, xtest, ytest= train_model(X,y)
-
-    space = {}
-    space['alpha']= [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100]
-    space['learning_rate']= ['constant', 'optimal', 'invscaling', 'adaptive']
-    space['loss']= ['squared_error', 'huber', 'epsilon_insensitive','squared_epsilon_insensitive']
-    space['penalty']=['l2', 'l1', 'elasticnet']
-    space['max_iter']= [750, 1000, 1250, 1500]
-
-    model_sgdr, xtrain, ytrain, xval, yval, xtest, ytest = train_model(X,y)
-    data_sets=[xtrain, ytrain, xval, yval, xtest, ytest]
-
-    custom_model, custom_param, custom_metrics = custom_tune_regression_model_hyperparameters(SGDRegressor, space, data_sets)
-    tune_model, tune_param, tune_metrics = tune_regression_model_hyperparameters(SGDRegressor, space, data_sets)
-
-    print ('Initial Results')
-    train_mse, train_score = calculate_scores(xtrain,ytrain,model_sgdr)
-    print(f'Train Set: RMSE: {train_mse**(1/2.0)}, score: {train_score}')
-    val_mse, val_score = calculate_scores(xval,yval,model_sgdr)
-    print(f'Val Set: RMSE: {val_mse**(1/2.0)}, score: {val_score}')
-    test_mse, test_score = calculate_scores(xtest,ytest,model_sgdr)
+    #model_sgdr, xtrain, ytrain, xval, yval, xtest, ytest= train_model(X,y)
+    #data_sets=[xtrain, ytrain, xval, yval, xtest, ytest]
+    model_param_dic={SGDRegressor:sgd_param, 
+        DecisionTreeRegressor:decision_tree_param, 
+        RandomForestRegressor:random_forest_param, 
+        GradientBoostingRegressor:gradient_boost_param}
     
-    print(f'Test Set: RMSE: {test_mse**(1/2.0)}, score: {test_score}')
-    print(f'Manual tuning: {custom_metrics}')
-    print(f'GridSearchCV tuning: {tune_metrics}')
+    #for item in model_param_dic.items():
+    #    evaluate_all_models(item[0], item[1], data_sets)
 
-#%%
+    model, param, metrics= find_best_model()
+    print(model, metrics)
 
+
+# %%
